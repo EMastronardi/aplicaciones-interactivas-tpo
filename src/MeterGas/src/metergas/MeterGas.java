088dfb74b41bcfea0a3baa50789dd9ef7ebd312f
emmanuelt88 <emmanuelt88@gmail.com>
2012-06-02 15:48:06 +0000

    Se agrega la gestión de vistas

--- src/MeterGas/src/metergas/MeterGas.java
+++ src/MeterGas/src/metergas/MeterGas.java
@@ -9,12 +9,5 @@ package metergas;
  * @author eteodoro
  */
 public class MeterGas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello world!");
-    }
+    
 }
