eae233f4062160ecb479eb3573e2a5a9cb650ce1
emolinero <emolinero@gmail.com>
2012-06-02 19:38:57 +0000

    Actualizacion de las clases Cliente y Medicion para contemplar los metodos de negocio

---
+++ src/MeterGas/src/metergas/model/views/ClienteIndustrialView.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metergas.model.views;
+
+/**
+ *
+ * @author Checho
+ */
+public class ClienteIndustrialView {
+
+    private String razonSocial;
+    private String nroIIBB;
+    private String condicionFiscal;
+    private String CUIT;
+
+    /**
+     * @return the razonSocial
+     */
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    /**
+     * @return the nroIIBB
+     */
+    public String getNroIIBB() {
+        return nroIIBB;
+    }
+
+    /**
+     * @return the condicionFiscal
+     */
+    public String getCondicionFiscal() {
+        return condicionFiscal;
+    }
+
+    /**
+     * @return the CUIT
+     */
+    public String getCUIT() {
+        return CUIT;
+    }
+}
