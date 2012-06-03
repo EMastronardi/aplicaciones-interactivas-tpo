62f43564d34eb43cbef953d998ae157254a3444c
emolinero <emolinero@gmail.com>
2012-06-03 19:41:46 +0000

    Actualizacion de las clases Cliente y Medicion para contemplar los metodos de negocio

--- src/MeterGas/src/metergas/model/LiquidadorResidencialSubsidiado.java
+++ src/MeterGas/src/metergas/model/LiquidadorResidencialSubsidiado.java
@@ -10,20 +10,27 @@ import java.util.Vector;
  *
  * @author chalom85
  */
-public class LiquidadorResidencialSubsidiado extends LiquidadorResidencial{
-    
-    public LiquidadorResidencialSubsidiado(){
+public class LiquidadorResidencialSubsidiado extends LiquidadorResidencial {
+
+    public LiquidadorResidencialSubsidiado() {
         this.conceptos = new Vector<Concepto>();
     }
-    
-    public Factura liquidar(Cliente c){
-        
-        
-        
-        
-        
-        return new Factura();
+
+    public Factura liquidar(Cliente c) {
+
+        Concepto topeSubsidio = buscarConcepto(ConceptoEnum.TOPESUBSIDIORESIDENCIAL.getTipoConcepto());
+        Concepto subsidio = buscarConcepto(ConceptoEnum.SUBSIDIORESIDENCIAL.getTipoConcepto());
+        float sub;
+        Factura factura = super.liquidarBase(c);
+
+
+        if (factura != null) {
+            if (factura.getTotal() < topeSubsidio.getValor()) {
+                sub = factura.getTotal() * -1 * (subsidio.getValor() / 100);
+                factura.generarItemFactura(subsidio.getConcepto(), sub);
+                return new Factura();
+            }
+        }
+        return null;
     }
-    
-    
 }
